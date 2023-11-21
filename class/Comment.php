1a2dd83b9123086288f90f14c312dee5552989df
ArkDev90 <larklatoja.development@gmail.com>
2023-11-21 18:33:33 +0800

    initial commit

---
+++ class/Comment.php
@@ -0,0 +1,42 @@
+<?php
+
+class Comment
+{
+	private $id; 
+	private $body; 
+	private $createdAt;
+	private $newsId;
+
+
+	public function __construct($id, $body, $createdAt, $newsId)
+    {
+        $this->id = $id;
+        $this->body = $body;
+        $this->createdAt = $createdAt;
+        $this->newsId = $newsId;
+    }
+
+
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getBody()
+	{
+		return $this->body;
+	}
+
+
+	public function getCreatedAt()
+	{
+		return $this->createdAt;
+	}
+
+	public function getNewsId()
+	{
+		return $this->newsId;
+	}
+
+}
---
+++ class/News.php
@@ -0,0 +1,44 @@
+<?php
+
+class News
+{
+
+	private $id;
+	private $title;
+	private $body;
+	private $createdAt;
+
+	
+	public function __construct($id, $title, $body, $createdAt)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->body = $body;
+        $this->createdAt = $createdAt;
+    }
+
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+
+
+	public function getBody()
+	{
+		return $this->body;
+	}
+
+	public function getCreatedAt()
+	{
+		return $this->createdAt;
+	}
+}
